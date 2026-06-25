3536608fa9c48b826b6ef54990c60b11e3e2f65d
Humeniuk <serhiy.hyma@gmail.com>
2026-06-25 06:38:06 +0300

    Split navigation and API UI e2e tests

---
+++ tests/e2e/articles-api-ui.serial.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from '@playwright/test';
+import { test } from '../../fixtures/app.fixture';
+
+
+test.describe.configure({mode: "serial"});
+
+test.beforeEach(async ({ homePage }) => {
+  await homePage.open();
+});
+
+test('User should see article created via API on UI', async ({ homePage, authApi, articlesApi, articlesPage }) => {
+    // Create variables
+    const uniqueTitle = `Test Article ${Date.now()}`;
+
+    // API setup
+    const token = await authApi.login();
+    await articlesApi.createArticle(token, uniqueTitle);
+
+    // Open UI
+    await homePage.open();
+    await homePage.openArticles();
+
+    // Assert created article is visible
+    await expect(articlesPage.articleTitle(uniqueTitle)).toBeVisible();
+});
+
+test('User should see article created via API on UI after DB restore', async ({ page, homePage, authApi, articlesApi, restoreApi, articlesPage }) => {
+    // Arrange
+    await restoreApi.restoreDefaultDB();
+    const uniqueTitle = `Restored Article ${Date.now()}`;
+    const token = await authApi.login();
+    await articlesApi.createArticle(token, uniqueTitle);
+    // Act
+    await homePage.openArticles();
+    // Assert
+    await expect(articlesPage.articleTitle(uniqueTitle)).toBeVisible();
+});
--- tests/ui/navigation.spec.ts
+++ tests/ui/navigation.spec.ts
@@ -1,9 +1,6 @@
 import { expect } from '@playwright/test';
 import { test } from '../../fixtures/app.fixture';
-import { ArticlesPage } from '../../pages/articles.page';
 
-
-test.describe.configure({mode: 'serial'});
 
 test.beforeEach(async ({ homePage }) => {
     await homePage.open();
@@ -30,32 +27,4 @@ test('User should see pagination on Articles page', async ({ page, homePage, art
     //Assert
     await expect(page).toHaveURL('http://localhost:3000/articles.html');
     await expect(articlesPage.pagination.root).toBeVisible();
-});
-
-test('User should see article created via API on UI', async ({ page, homePage, authApi, articlesApi, articlesPage }) => {
-    // Create variables
-    const uniqueTitle = `Test Article ${Date.now()}`;
-
-    // API setup
-    const token = await authApi.login();
-    await articlesApi.createArticle(token, uniqueTitle);
-
-    // Open UI
-    await homePage.open();
-    await homePage.openArticles();
-
-    // Assert created article is visible
-    await expect(articlesPage.articleTitle(uniqueTitle)).toBeVisible();
-});
-
-test('User should see article created via API on UI after DB restore', async ({ page, homePage, authApi, articlesApi, restoreApi, articlesPage }) => {
-    // Arrange
-    await restoreApi.restoreDefaultDB();
-    const uniqueTitle = `Restored Article ${Date.now()}`;
-    const token = await authApi.login();
-    await articlesApi.createArticle(token, uniqueTitle);
-    // Act
-    await homePage.openArticles();
-    // Assert
-    await expect(articlesPage.articleTitle(uniqueTitle)).toBeVisible();
 });
