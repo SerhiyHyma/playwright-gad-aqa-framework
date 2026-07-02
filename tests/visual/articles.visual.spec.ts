07c2f7f6084cedeb83056c210e16c6bee4414df1
Humeniuk <serhiy.hyma@gmail.com>
2026-07-02 21:13:45 +0300

    Prettier applied

--- tests/visual/articles.visual.spec.ts
+++ tests/visual/articles.visual.spec.ts
@@ -1,16 +1,15 @@
-import { expect } from "@playwright/test";
-import { test } from "../../fixtures/app.fixture";
+import { expect } from '@playwright/test';
+import { test } from '../../fixtures/app.fixture';
 
+test.describe.configure({ mode: 'serial' });
 
-test.describe.configure({mode: 'serial'});
-
-test.beforeEach(async ({restoreApi}) => {
-    await restoreApi.restoreDefaultDB();
+test.beforeEach(async ({ restoreApi }) => {
+  await restoreApi.restoreDefaultDB();
 });
 
-test("Articles page should match visual baseline", async ({articlesPage, page}) => {
-    await articlesPage.open();
-    await expect(page).toHaveScreenshot('articles-page.png', {
-        fullPage: true,
-    });
+test('Articles page should match visual baseline', async ({ articlesPage, page }) => {
+  await articlesPage.open();
+  await expect(page).toHaveScreenshot('articles-page.png', {
+    fullPage: true,
+  });
 });
