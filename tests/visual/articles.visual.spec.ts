020a081dbaa42603ea23d40049228421142c4699
Humeniuk <serhiy.hyma@gmail.com>
2026-07-02 21:04:35 +0300

    Add visual test for articles page

---
+++ tests/visual/articles.visual.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/app.fixture";
+
+
+test.describe.configure({mode: 'serial'});
+
+test.beforeEach(async ({restoreApi}) => {
+    await restoreApi.restoreDefaultDB();
+});
+
+test("Articles page should match visual baseline", async ({articlesPage, page}) => {
+    await articlesPage.open();
+    await expect(page).toHaveScreenshot('articles-page.png', {
+        fullPage: true,
+    });
+});
